bce5d074b0df16dd25561dc583399857618328a5
ziedlassoued <zied.lassoued@outlook.de>
2021-10-08 14:09:08 +0200

    Add recipes

---
+++ src/components/recipes/recipes.stories.js
@@ -0,0 +1,9 @@
+import "./recipes.css";
+import recipes from "./recipes.html";
+
+export default {
+  title: "Components/Recipes",
+  parameters: { layout: "centered" },
+};
+
+export const recipe = () => recipes;
